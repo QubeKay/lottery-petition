1c5c1f1038a0a58120d7b0c3e4fc51769be53f95
Kelvin Chot <kinyunye@gmail.com>
2023-01-14 12:44:37 +0300

    Initial commit: start with default NextJS starter template

---
+++ pages/posts/first-post.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+// import Script from 'next/script';
+
+export default function FirstPost() {
+  return (
+    <>
+    <Head>
+        <title>First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <Image
+        src="/images/profile.png" // Route of the image file
+        height={333} // Desired size with correct aspect ratio
+        width={604} // Desired size with correct aspect ratio
+        alt="Gachunia Profile Pic"
+        />
+    </>
+  );
+}
